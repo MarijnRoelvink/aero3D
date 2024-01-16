e10f67d72241612fad66dd9cf8562575e2d7643a
Marijn Roelvink <m.a.t.roelvink@student.tudelft.nl>
2024-01-16 17:09:10 +0100

    Add highlights

--- main.js
+++ main.js
@@ -1,54 +1,44 @@
-
 import * as THREE from 'three';
 
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 let camera, scene, renderer, controls, lights, o_center;
 let l_distance = 1;
-let dir =  'models/'
+let dir = 'models/'
 
 route();
 
 function route() {
 	let model = getUrlQuery("model");
+	let highlight = getUrlQuery("highlight");
 	fetch(`config.json`)
 		.then((response) => response.json())
 		.then((json) => {
 			let config = json.find(item => item.number === model);
-			init(config);
+			let h_config = highlight !== "" ? json.find(item => item.number === highlight) : "";
+			init(config, h_config);
 			animate();
 		});
 }
 
-function updateLights() {
-	let forward = new THREE.Vector3( );
-	let v = getVaxis(THREE, camera);
-	camera.getWorldDirection(forward);
-	forward.negate()
-		.normalize()
-		.multiplyScalar(l_distance);
+function init(config, h_config) {
 
-	let key = forward.clone().applyAxisAngle(v, -45.0/360.0*2*Math.PI).add(o_center);
-	let fill = forward.clone().applyAxisAngle(v, 45.0/360.0*2*Math.PI).add(o_center);
-	lights[0].position.copy(key);
-	lights[1].position.copy(fill);
-}
-
-function init(config) {
-
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 20 );
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 20);
 	camera.position.z = 2.5;
 
 	// scene
 
 	scene = new THREE.Scene();
-	
+
+	const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+	scene.add(ambientLight);
+
 	let light_configs = [{color: 0xffffff, intensity: 3},
 		{color: 0xa3aeff, intensity: 2}]
 	lights = [];
-	for (let l of light_configs)  {
-		let light = new THREE.PointLight( l.color, l.intensity, 0 );
+	for (let l of light_configs) {
+		let light = new THREE.PointLight(l.color, l.intensity, 0);
 		scene.add(light);
 		lights.push(light);
 		// const sphereSize = 0.1;
@@ -56,13 +46,13 @@ function init(config) {
 		// scene.add( pointLightHelper );
 	}
 
-	scene.add( camera );
+	scene.add(camera);
 
 	// model
 
-	const onProgress = function ( xhr ) {
+	const onProgress = function (xhr) {
 
-		if ( xhr.lengthComputable ) {
+		if (xhr.lengthComputable) {
 
 			const percentComplete = xhr.loaded / xhr.total * 100;
 			// console.log( percentComplete.toFixed( 2 ) + '% downloaded' );
@@ -71,36 +61,89 @@ function init(config) {
 	};
 
 	//
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
 
 	//
 
-	controls = new  OrbitControls( camera, renderer.domElement );
+	controls = new OrbitControls(camera, renderer.domElement);
 	controls.minDistance = 0.5;
 	controls.maxDistance = 5;
 	controls.addEventListener('change', updateLights);
 
-	var loader = new GLTFLoader().setPath( dir );
-	loader.load( `${config.model_name}.glb`, function ( gltf )
-		{
-			let object = gltf.scene;
-			scale_to_screen(THREE, object);
-			let box = new THREE.Box3().setFromObject( object );
-			o_center = new THREE.Vector3();
-			box.getCenter( o_center );
-			controls.target = o_center;
-			camera.lookAt(o_center);
-			updateLights();
-			scene.add( object );
-		});
+	var loader = new GLTFLoader().setPath(dir);
+	loader.load(`${config.model_name}.glb`, function (gltf) {
+		let object = gltf.scene;
+		scale_to_screen(THREE, object);
+		setObjectMaterial(object);
+		add_highlight(h_config, object);
+		let box = new THREE.Box3().setFromObject(object);
+		o_center = new THREE.Vector3();
+		box.getCenter(o_center);
+		controls.target = o_center;
+		camera.lookAt(o_center);
+		updateLights();
+		scene.add(object);
+	});
 
 	//
 
-	window.addEventListener( 'resize', onWindowResize );
+	window.addEventListener('resize', onWindowResize);
 
+}
+
+function setObjectMaterial(object, highlight = false) {
+	let standardColor = 0xB5B5B5;
+	let highlightColor = 0x1A7A50;
+
+	let standardMtl = new THREE.MeshPhongMaterial({
+		color: standardColor,
+		specular: standardColor,
+		side: THREE.DoubleSide,
+		shininess: 100,
+	});
+
+	let highlightMtl = new THREE.MeshPhongMaterial({
+		color: highlightColor,
+		specular: highlightColor,
+		side: THREE.DoubleSide,
+		shininess: 5
+	});
+
+	object.traverse(o => {
+		if (o.isMesh) {
+			console.log(o.material);
+			o.material = highlight? highlightMtl: standardMtl;
+		}
+	});
+	return object;
+}
+
+function updateLights() {
+	let forward = new THREE.Vector3();
+	let v = getVaxis(THREE, camera);
+	camera.getWorldDirection(forward);
+	forward.negate()
+		.normalize()
+		.multiplyScalar(l_distance);
+
+	let key = forward.clone().applyAxisAngle(v, -45.0 / 360.0 * 2 * Math.PI).add(o_center);
+	let fill = forward.clone().applyAxisAngle(v, 45.0 / 360.0 * 2 * Math.PI).add(o_center);
+	lights[0].position.copy(key);
+	lights[1].position.copy(fill);
+}
+
+function add_highlight(h_config, object) {
+	if(h_config === "") {
+		return;
+	}
+	object.traverse(child => {
+		if(child.name === h_config.model_name) {
+			setObjectMaterial(child, true);
+		}
+	})
 }
 
 function onWindowResize() {
@@ -108,7 +151,7 @@ function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
 
@@ -116,8 +159,8 @@ function onWindowResize() {
 
 function animate() {
 
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
 
 }
 
